fcfeeb4272b11e9c6cf061c5c095dfdb983bd20b
Guillaume <Guillaume>
2023-08-23 22:52:46 +0200

    Added dist/

---
+++ dist/harmony-browser-utils.js
@@ -0,0 +1,9 @@
+function SaveFile(file) {
+	var link = document.createElement('a');
+	link.setAttribute('href', URL.createObjectURL(file));
+	link.setAttribute('download', file.name);
+
+	link.click();
+}
+
+export { SaveFile };
