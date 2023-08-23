381aa69873cc1801220c539a6754b695d86a4aba
Guillaume <Guillaume>
2023-08-23 22:47:21 +0200

    Initial commit

---
+++ rollup.config.js
@@ -0,0 +1,17 @@
+import postcss from 'rollup-plugin-postcss';
+import {nodeResolve} from '@rollup/plugin-node-resolve';
+
+export default [
+	{
+		input: './src/index.js',
+		output: {
+			file: './dist/harmony-browser-utils.js',
+			format: 'esm'
+		},
+		plugins: [
+			postcss({
+			}),
+			nodeResolve(),
+		],
+	},
+];
