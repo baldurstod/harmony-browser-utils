d7b5e49772084623f238ece522b9ad655bb6b9c5
Guillaume <Guillaume>
2023-09-16 14:25:43 +0200

    Uses harmony-ui

--- src/notificationmanager.js
+++ src/notificationmanager.js
@@ -1,9 +1,12 @@
+import { createElement, hide, show } from 'harmony-ui';
+
 const NOTIFICATION_CLASSNAME = 'notification-manager-notification';
 const CLOSE_SVG = '<svg viewBox="0 0 357 357"><polygon points="357,35.7 321.3,0 178.5,142.8 35.7,0 0,35.7 142.8,178.5 0,321.3 35.7,357 178.5,214.2 321.3,357 357,321.3 214.2,178.5"/></svg>';
 
 import './css/notificationmanager.css';
 
 class Notification {
+	#htmlElement;
 	constructor(content, type, ttl) {
 		this.content = content;
 		this.type = type;
@@ -18,33 +21,42 @@ class Notification {
 	}
 
 	get view() {
-		if (!this.htmlElement) {
-			this.htmlElement = document.createElement('div');
-			let htmlElementContent = document.createElement('div');
-			htmlElementContent.className = NOTIFICATION_CLASSNAME + '-content';
-			let htmlElementClose = document.createElement('div');
-			htmlElementClose.className = NOTIFICATION_CLASSNAME + '-close';
+		if (!this.#htmlElement) {
+			let htmlElementContent;
+			this.#htmlElement = createElement('div', {
+				className: NOTIFICATION_CLASSNAME,
+				childs: [
+					htmlElementContent = createElement('div', {
+						className: NOTIFICATION_CLASSNAME + '-content',
+					}),
+					createElement('div', {
+						className: NOTIFICATION_CLASSNAME + '-close',
+						innerHTML: CLOSE_SVG,
+						events: {
+							click: () => NotificationManager.closeNofication(this),
+						}
+					}),
+				]
+			});
 
-			this.htmlElement.append(htmlElementContent, htmlElementClose);
-			this.htmlElement.className = NOTIFICATION_CLASSNAME;
 			if (this.type) {
-				this.htmlElement.classList.add(NOTIFICATION_CLASSNAME + '-' + this.type)
+				this.#htmlElement.classList.add(NOTIFICATION_CLASSNAME + '-' + this.type)
 
 			}
+
 			if (this.content instanceof HTMLElement) {
-			htmlElementContent.append(this.content);
+				htmlElementContent.append(this.content);
 			} else {
 				htmlElementContent.innerHTML = this.content;
 			}
-			htmlElementClose.innerHTML = CLOSE_SVG;
-			htmlElementClose.addEventListener('click', () => NotificationManager.closeNofication(this));
 		}
-		return this.htmlElement;
+		return this.#htmlElement;
 	}
 }
 
 export const NotificationManager = new (function () {
 	class NotificationManager extends EventTarget {//TODOv3 are we going to send events ?
+		#htmlElement;
 		constructor() {
 			super();
 			this.htmlParent = document.body;
@@ -54,16 +66,16 @@ export const NotificationManager = new (function () {
 
 		set parent(htmlParent) {
 			this.htmlParent = htmlParent;
-			this.htmlParent.append(this.htmlElement);
+			this.htmlParent.append(this.#htmlElement);
 		}
 
 		createHtml() {
-			this.htmlElement = document.createElement('div');
-			this.htmlElement.className = 'notification-manager';
-			this.htmlParent.append(this.htmlElement);
+			this.#htmlElement = document.createElement('div');
+			this.#htmlElement.className = 'notification-manager';
+			this.htmlParent.append(this.#htmlElement);
 		}
 
-		_getNotification(content, type, ttl) {
+		#getNotification(content, type, ttl) {
 			for (let notification of this.nofifications) {
 				if ((notification.content ==content) && (notification.type == type)) {
 					notification.ttl = ttl;
@@ -74,9 +86,9 @@ export const NotificationManager = new (function () {
 		}
 
 		addNotification(content, type, ttl) {
-			let notification = this._getNotification(content, type, ttl);
+			let notification = this.#getNotification(content, type, ttl);
 			this.nofifications.add(notification);
-			this.htmlElement.append(notification.view);
+			this.#htmlElement.append(notification.view);
 		}
 
 		closeNofication(notification) {
