678050025ea7e884f4701f98f18fa690a5fa740e
Guillaume <108530065+baldurstod@users.noreply.github.com>
2024-11-07 23:35:50 +0100

    Added typescript declarations

--- src/ts/notificationmanager.ts
+++ src/ts/notificationmanager.ts
@@ -1,19 +1,23 @@
-import { createElement, shadowRootStyle, I18n } from 'harmony-ui';
+import { createElement, shadowRootStyle, I18n, createShadowRoot } from 'harmony-ui';
 import { closeSVG, contentCopySVG } from 'harmony-svg';
+import notificationManagerCSS from '../css/notificationmanager.css';
 
 const NOTIFICATION_CLASSNAME = 'notification-manager-notification';
 
-import notificationManagerCSS from './css/notificationmanager.css';
+export type NotificationContent = HTMLElement | string;
 
 class Notification {
-	#htmlElement;
-	constructor(content, type, ttl) {
+	#htmlElement?: HTMLElement;
+	timeout: number = 0;
+	content: NotificationContent;
+	type: string;
+	constructor(content: NotificationContent, type: string, ttl?: number) {
 		this.content = content;
 		this.type = type;
-		this.ttl = ttl;
+		this.setTtl(ttl);
 	}
 
-	set ttl(ttl) {
+	setTtl(ttl?: number) {
 		if (ttl) {
 			clearTimeout(this.timeout);
 			this.timeout = setTimeout(() => NotificationManager.closeNofication(this), ttl * 1000);
@@ -33,10 +37,12 @@ class Notification {
 						className: NOTIFICATION_CLASSNAME + '-copy',
 						innerHTML: contentCopySVG,
 						events: {
-							click: async (event) => {
+							click: async (event: Event) => {
 								try {
-									await navigator.clipboard.writeText(this.#htmlElement.innerText);
-									event.target.parentElement.classList.toggle(NOTIFICATION_CLASSNAME + '-copy-success');
+									if (this.#htmlElement) {
+										await navigator.clipboard.writeText(this.#htmlElement.innerText);
+										(event.target as HTMLElement).parentElement?.classList.toggle(NOTIFICATION_CLASSNAME + '-copy-success');
+									}
 								} catch (e) {
 									console.error(e);
 								}
@@ -68,46 +74,47 @@ class Notification {
 }
 
 export class NotificationManager {
-	static #htmlElement;
+	//static #htmlElement: HTMLElement;
 	static #htmlParent = document.body;
-	static #shadowRoot;
-	static #nofifications = new Set();
+	static #shadowRoot: ShadowRoot;
+	static #nofifications = new Set<Notification>();
 	static {
 		this.#createHtml();
 	}
 
-	static set parent(htmlParent) {
+	static setParent(htmlParent: HTMLElement) {
 		this.#htmlParent = htmlParent;
-		this.#htmlParent.append(this.#htmlElement);
+		this.#htmlParent.append(this.#shadowRoot.host);
 	}
 
 	static #createHtml() {
-		this.#htmlElement = createElement('div', {
+		this.#shadowRoot = createShadowRoot('div', {
 			class: 'notification-manager',
 			parent: this.#htmlParent,
+			adoptStyle: notificationManagerCSS,
 		});
-		this.#shadowRoot = this.#htmlElement.attachShadow({ mode: 'closed' });
-		shadowRootStyle(this.#shadowRoot, notificationManagerCSS);
+		//this.#shadowRoot = this.#htmlElement.attachShadow({ mode: 'closed' });
+		//shadowRootStyle(this.#shadowRoot, notificationManagerCSS);
 		I18n.observeElement(this.#shadowRoot);
 	}
 
-	static #getNotification(content, type, ttl) {
+	static #getNotification(content: NotificationContent, type: string, ttl: number) {
 		for (let notification of this.#nofifications) {
-			if ((notification.content ==content) && (notification.type == type)) {
-				notification.ttl = ttl;
+			if ((notification.content == content) && (notification.type == type)) {
+				notification.setTtl(ttl);
 				return notification;
 			}
 		}
 		return new Notification(content, type, ttl);
 	}
 
-	static addNotification(content, type, ttl) {
+	static addNotification(content: NotificationContent, type: string, ttl: number) {
 		let notification = this.#getNotification(content, type, ttl);
 		this.#nofifications.add(notification);
 		this.#shadowRoot.append(notification.view);
 	}
 
-	static closeNofication(notification) {
+	static closeNofication(notification: Notification) {
 		this.#nofifications.delete(notification);
 		notification.view.remove();
 	}
