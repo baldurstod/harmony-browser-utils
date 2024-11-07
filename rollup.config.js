896ca6651071503bf0828e5b07964da73fe73617
Guillaume <108530065+baldurstod@users.noreply.github.com>
2024-11-08 00:54:27 +0100

    Added typescript build

--- rollup.config.js
+++ rollup.config.js
@@ -1,15 +1,43 @@
+import fs from 'fs';
+import child_process from 'child_process';
+import typescript from '@rollup/plugin-typescript';
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+import css from 'rollup-plugin-import-css';
 
-import {nodeResolve} from '@rollup/plugin-node-resolve';
+const TEMP_BUILD = './dist/dts/index.js';
 
 export default [
 	{
-		input: './src/index.js',
+		input: './src/index.ts',
 		output: {
-			file: './dist/harmony-browser-utils.js',
+			file: TEMP_BUILD,
 			format: 'esm'
 		},
 		plugins: [
+			css(),
 			nodeResolve(),
+			typescript(),
+			/*
+			{
+				name: 'postbuild-commands',
+				closeBundle: async () => {
+					await postBuildCommands()
+				}
+			},
+			*/
 		],
 	},
 ];
+
+async function postBuildCommands() {
+	fs.copyFile(TEMP_BUILD, './dist/index.js', err => { if (err) throw err });
+	return new Promise(resolve => child_process.exec(
+		'api-extractor run --local --verbose --typescript-compiler-folder ./node_modules/typescript',
+		(error, stdout, stderr) => {
+			if (error) {
+				console.log(error);
+			}
+			resolve("done")
+		},
+	));
+}
